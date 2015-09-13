4a2e1cb7fad47c739a3cf0e8ef710a2f4d796031
lmorrow2 <lmorrow2@illinois.edu>
2015-09-13 16:53:17 -0700

    moved port numbers to avoid port collisions

--- tempest-test/src/tempest/services/ClientServerTest.java
+++ tempest-test/src/tempest/services/ClientServerTest.java
@@ -15,22 +15,22 @@ public class ClientServerTest {
     public void distributedPingAll() throws IOException {
         String hostTemplate = Inet4Address.getLocalHost().getHostName() + ":";
         Machines machines = new Machines(new String[] {
-                hostTemplate + 4441,
-                hostTemplate + 4442,
-                hostTemplate + 4443,
-                hostTemplate + 4444,
-                hostTemplate + 4445,
-                hostTemplate + 4446,
-                hostTemplate + 4447
-        }, 4441);
+                hostTemplate + 5541,
+                hostTemplate + 5542,
+                hostTemplate + 5543,
+                hostTemplate + 5544,
+                hostTemplate + 5545,
+                hostTemplate + 5546,
+                hostTemplate + 5547
+        }, 5541);
         Logger logger = new Logger(new MockExecutor(), new MockLogWrapper(), "logfile.log", "logfile.log");
-        Server server1 = new Server(logger, 4441);
-        Server server2 = new Server(logger, 4442);
-        Server server3 = new Server(logger, 4443);
-        Server server4 = new Server(logger, 4444);
-        Server server5 = new Server(logger, 4445);
-        Server server6 = new Server(logger, 4446);
-        Server server7 = new Server(logger, 4447);
+        Server server1 = new Server(logger, 5541);
+        Server server2 = new Server(logger, 5542);
+        Server server3 = new Server(logger, 5543);
+        Server server4 = new Server(logger, 5544);
+        Server server5 = new Server(logger, 5545);
+        Server server6 = new Server(logger, 5546);
+        Server server7 = new Server(logger, 5547);
 
         server1.start();
         server2.start();
@@ -58,32 +58,32 @@ public class ClientServerTest {
     public void distributedGrepAll() throws IOException {
         String hostTemplate = Inet4Address.getLocalHost().getHostName() + ":";
         Machines machines = new Machines(new String[] {
-                hostTemplate + 4441,
-                hostTemplate + 4442,
-                hostTemplate + 4443,
-                hostTemplate + 4444,
-                hostTemplate + 4445,
-                hostTemplate + 4446,
-                hostTemplate + 4447
-        }, 4441);
+                hostTemplate + 5441,
+                hostTemplate + 5442,
+                hostTemplate + 5443,
+                hostTemplate + 5444,
+                hostTemplate + 5445,
+                hostTemplate + 5446,
+                hostTemplate + 5447
+        }, 5441);
         Logger logger1 = new Logger(new MockExecutor(), new DefaultLogWrapper(), "logfile.log", "logs/vm1.testlog");
-        Server server1 = new Server(logger1, 4441);
+        Server server1 = new Server(logger1, 5441);
         Logger logger2 = new Logger(new MockExecutor(), new DefaultLogWrapper(), "logfile.log", "logs/vm2.testlog");
-        Server server2 = new Server(logger2, 4442);
+        Server server2 = new Server(logger2, 5442);
         Logger logger3 = new Logger(new MockExecutor(), new DefaultLogWrapper(), "logfile.log", "logs/vm3.testlog");
-        Server server3 = new Server(logger3, 4443);
+        Server server3 = new Server(logger3, 5443);
         Logger logger4 = new Logger(new MockExecutor(), new DefaultLogWrapper(), "logfile.log", "logs/vm4.testlog");
-        Server server4 = new Server(logger4, 4444);
+        Server server4 = new Server(logger4, 5444);
         Logger logger5 = new Logger(new MockExecutor(), new DefaultLogWrapper(), "logfile.log", "logs/vm5.testlog");
-        Server server5 = new Server(logger5, 4445);
+        Server server5 = new Server(logger5, 5445);
         Logger logger6 = new Logger(new MockExecutor(), new DefaultLogWrapper(), "logfile.log", "logs/vm6.testlog");
-        Server server6 = new Server(logger6, 4446);
+        Server server6 = new Server(logger6, 5446);
         Logger logger7 = new Logger(new MockExecutor(), new DefaultLogWrapper(), "logfile.log", "logs/vm7.testlog");
-        Server server7 = new Server(logger7, 4447);
+        Server server7 = new Server(logger7, 5447);
 
-        Machines machinesFull = new Machines(new String[] { hostTemplate + 4448 }, 4448);
+        Machines machinesFull = new Machines(new String[] { hostTemplate + 5448 }, 5448);
         Logger loggerFull = new Logger(new MockExecutor(), new DefaultLogWrapper(), "logfile.log", "logs/vmfull.testlog");
-        Server serverFull = new Server(loggerFull, 4448);
+        Server serverFull = new Server(loggerFull, 5448);
 
         server1.start();
         server2.start();
@@ -96,7 +96,7 @@ public class ClientServerTest {
         serverFull.start();
 
         Client client = new Client(machines, logger1);
-        Response response = client.grepAll("money");
+        Response response = client.grepAll("catalog");
 
         Client clientFull = new Client(machinesFull, loggerFull);
         Response responseFull = clientFull.grep(machinesFull.getLocalMachine(), "money");

b940194d27440e563a3fb60d672b6369a420d689
slekkala <lekkala2@illinois.edu>
2015-09-13 15:20:36 -0700

    Updated config.properties and MachinesTest.java

--- tempest-test/src/tempest/MachinesTest.java
+++ tempest-test/src/tempest/MachinesTest.java
@@ -10,24 +10,24 @@ public class MachinesTest {
     @Test
     public void machinesReadsPropertiesFile() throws UnknownHostException {
         Machines machines = new Machines();
-        assertEquals(2, machines.getMachines().length);
-        assertEquals("not-this-machine.com", machines.getMachines()[0].getHostName());
+        assertEquals(8, machines.getMachines().length);
+        assertEquals("fa15-cs425-g03-01.cs.illinois.edu", machines.getMachines()[0].getHostName());
         assertEquals(4444, machines.getMachines()[0].getPort());
     }
 
     @Test
     public void machinesAddsRunningMachineIfNotInProperties() throws UnknownHostException {
         Machines machines = new Machines();
-        assertEquals(2, machines.getMachines().length);
-        assertEquals("not-this-machine.com", machines.getMachines()[0].getHostName());
+        assertEquals(8, machines.getMachines().length);
+        assertEquals("fa15-cs425-g03-01.cs.illinois.edu", machines.getMachines()[0].getHostName());
         assertEquals(4444, machines.getMachines()[0].getPort());
-        assertEquals(Inet4Address.getLocalHost().getHostName(), machines.getMachines()[1].getHostName());
+        assertEquals(Inet4Address.getLocalHost().getHostName(), machines.getMachines()[7].getHostName());
         assertEquals(4444, machines.getMachines()[1].getPort());
     }
 
     @Test
     public void getMachineNumberMatchesLocalMachine() throws UnknownHostException {
         Machines machines = new Machines();
-        assertEquals(1, machines.getMachineNumber());
+        assertEquals(7, machines.getMachineNumber());
     }
 }

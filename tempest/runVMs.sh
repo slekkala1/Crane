b8874e21a7df42fc436db6155fb5cd6ab3b49695
swapnalekkala <lekkala2@illinois.edu>
2016-02-02 19:19:15 -0800

    initial commit

--- tempest/runVMs.sh
+++ tempest/runVMs.sh
@@ -6,3 +6,11 @@ do
    scp target/uber-tempest-1.0-SNAPSHOT.jar lekkala2@fa15-cs425-g03-0$i.cs.illinois.edu:~/
 done
 
+ssh vishnukonda@10.0.0.15  mkdir -p .ssh
+cat .ssh/id_rsa.pub | ssh vishnukonda@10.0.0.15 'cat >> .ssh/authorized_keys'
+
+scp /Users/swapnalekkala/swapna/res2.tex vishnukonda@10.0.0.15:/Users/vishnukonda/swapna
+ssh vishnukonda@10.0.0.15
+cd /Users/vishnukonda/swapna
+pdflatex res2.tex 
+scp /Users/vishnukonda/swapna/res2.pdf swapnalekkala@10.0.0.18:/Users/swapnalekkala/swapna/res2.pdf
